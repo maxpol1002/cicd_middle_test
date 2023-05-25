516cb0af573914f806cc69553204d6c72a5f3bde
maxpol <mpoluyk@gmail.com>
2023-05-25 11:24:03 +0300

    tests.py file updated

--- recipe/tests.py
+++ recipe/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Recipe
 
-# Create your tests here.
+
+class RecipeTestCase(TestCase):
+    def setUp(self):
+        self.recipe = Recipe.objects.create(
+            title='Test Recipe',
+            description='This is a test recipe',
+            instructions='Test instructions',
+            ingredients='Test ingredients'
+        )
+
+    def test_recipe_detail_view(self):
+        url = reverse('recipe:recipe_detail', args=[self.recipe.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.recipe.title)
+        self.assertContains(response, self.recipe.description)
+        self.assertContains(response, self.recipe.instructions)
+        self.assertContains(response, self.recipe.ingredients)
+
+    def test_main_view(self):
+        url = reverse('recipe:main')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['recipes'], [repr(self.recipe)])
